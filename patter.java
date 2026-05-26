2ede0574ef95e8157f7747b9b3792b4a8dcb3219
mallappabiradar <mallappabiradar064@gmail.com>
2026-05-26 23:23:39 +0530

    first commit

---
+++ patter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class patter{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n value");
+        int n = sc.nextInt();
+        int i,j;
+        for(i=0;i<=n;i++){
+            for(j=0;i<=i;j++){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
