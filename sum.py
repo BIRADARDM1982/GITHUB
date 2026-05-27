2bbd2ae5f41182e52a2cf3df632d7609fe15c445
mallappabiradar <mallappabiradar064@gmail.com>
2026-05-27 09:15:28 +0530

    MP3 UPLOADED PROJECT FILES

--- sum.py
+++ sum.py
@@ -1,5 +1,5 @@
 a=2
 b=4
 c = 7
-print("a+b+c = ", a+b+c);
-print("a.b.c = ", a*b*c);
+print("a+b+c = ", a+b+c);             
+print("a.b.c = ", a*b*c);                                                                                                                     
